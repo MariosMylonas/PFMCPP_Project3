2be9a47e53f42553a8a34291d437b759d0f48aac
MariosMylonas <128589233+MariosMylonas@users.noreply.github.com>
2023-04-26 17:45:21 +0000

    UDTwithUDTs1c

--- main.cpp
+++ main.cpp
@@ -337,17 +337,17 @@ Define an object that is made of 5 sub-objects.
     These 5 sub-objects will not be defined using Primitives, but instead will be their own UDTs 
     you'll define these 5 sub-objects in Part 1d.
 
-Thing 10)
+Thing 10) audiovisual company
 5 properties:
-    1)
-    2)
-    3)
-    4)
-    5)
+    1) audio department
+    2) visual department
+    3) sales
+    4) RnD department
+    5) Finance and marketing
 3 things it can do:
-    1)
-    2)
-    3)
+    1) create audiovisual content
+    2) develop new audiovisual software
+    3) sell products to customers
  */
 
  /*
